58c78a5325bbedd21349b0176073dc791c0a60e6
ylndz13 <xz886@cornell.edu>
2025-07-13 23:43:00 -0400

    Fixed redefinition issue

--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -1,10 +1,6 @@
 #include "Vector3.h"
 #include <math.h>
 
-// Constructors
-Vector3::Vector3() : x(0), y(0), z(0) {}
-Vector3::Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
-
 // Vector operations
 float Vector3::magnitude() const {
     return sqrt(x * x + y * y + z * z);
