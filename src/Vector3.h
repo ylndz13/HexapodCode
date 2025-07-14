b1e8446b67ff1a7f2ed9a873906c23e4d2f511d5
ylndz13 <xz886@cornell.edu>
2025-07-13 23:41:49 -0400

    Fixed a redefinition bug

--- src/Vector3.h
+++ src/Vector3.h
@@ -5,20 +5,20 @@ class Vector3 {
     public: 
         float x, y, z;
 
-    // Constructors
-    Vector3(); 
-    Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+        // Constructors
+        Vector3() : x(0), y(0), z(0) {}
+        Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 
-    // Vector operations
-    float magnitude() const;
-    Vector3 distance(const Vector3& other) const;
+        // Vector operations
+        float magnitude() const;
+        Vector3 distance(const Vector3& other) const;
 
-    Vector3 cross(const Vector3& other) const;
-    float dot(const Vector3& other) const;
+        Vector3 cross(const Vector3& other) const;
+        float dot(const Vector3& other) const;
 
-    // Operator overloads
-    Vector3 operator+(const Vector3& other) const;
-    Vector3 operator-(const Vector3& other) const;
+        // Operator overloads
+        Vector3 operator+(const Vector3& other) const;
+        Vector3 operator-(const Vector3& other) const;
 };
 
 #endif // VECTOR3_H
